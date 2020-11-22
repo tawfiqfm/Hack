1e41bce5627caa5f50cf83f882c391f92644ea2e
tawfiqfm <71957859+tawfiqfm@users.noreply.github.com>
2020-11-22 04:05:19 -0500

    Add files via upload

---
+++ Data.xml.sql
@@ -0,0 +1,10 @@
+/*
+-- Query: select * from attribute
+LIMIT 0, 1000
+
+-- Date: 2020-11-22 04:04
+*/
+INSERT INTO `` (`id`,`race`,`gender`,`history`,`film`,`sport`) VALUES (1,'White','Male',25,25,50);
+INSERT INTO `` (`id`,`race`,`gender`,`history`,`film`,`sport`) VALUES (2,'Asian','Female',35,30,35);
+INSERT INTO `` (`id`,`race`,`gender`,`history`,`film`,`sport`) VALUES (3,'White','Female',25,25,50);
+INSERT INTO `` (`id`,`race`,`gender`,`history`,`film`,`sport`) VALUES (4,'White','Male',70,10,20);
